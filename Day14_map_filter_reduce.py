1c2e1e593928b6fa65668c1ab8be9190125878e4
unknown <shubhamkadam19@gmail.com>
2020-07-28 17:03:00 +0530

    Day 14

---
+++ Day14_map_filter_reduce.py
@@ -0,0 +1,25 @@
+from functools import reduce
+
+list1=[1,2,3,4,5,6,7,8,9,10,11]
+list2=[100,102,103,104,105,106]
+list3=[1.33456243,12.3214534342,13.435546654,15.213243535]
+
+
+def greater_10(ls):
+    return ls > 10
+
+def custom_sum(first,second):
+    return first + second
+
+print(greater_10(int(100)))
+
+#we can also use a map(function,*iterables)
+print(list(map(greater_10,list1)))
+print(list(map(greater_10,list2)))
+
+
+print(list(map(round,list3,range(1,len(list3)+1))))
+
+print(list(filter(greater_10,list1)))
+
+print(reduce(custom_sum,list1))
