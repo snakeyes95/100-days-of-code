5e5b42d529daea714cb7ea56cf75b975b3a7c819
unknown <shubhamkadam19@gmail.com>
2020-07-30 16:53:33 +0530

    Day 16

---
+++ Day16_any_all_operators.py
@@ -0,0 +1,5 @@
+#the any and all operators are basically performing a series of OR and AND operations on the entire iterable
+
+
+print(any([True,True,True,False,False]))#The any operator is performing a set of OR operations on the iterable .
+print(all([True,True,True,False,False]))#The all operator is performing a set of AND operators on the iterable .
