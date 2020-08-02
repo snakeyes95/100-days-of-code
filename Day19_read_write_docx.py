7dc935d519a9ae66c5e12fc6a03f1c592939dc82
unknown <shubhamkadam19@gmail.com>
2020-08-02 17:22:07 +0530

    Day 19

---
+++ Day19_read_write_docx.py
@@ -0,0 +1,12 @@
+import docx
+
+document=docx.Document('sample_text.docx')
+
+all_paragraphs=document.paragraphs
+
+for para in all_paragraphs:
+    print(para)
+
+single_para = document.paragraphs[0]
+for run in single_para.runs:
+    print(run.text)
