c3f6353c62118eeb91c86105b52774cb36ffe495
shubham <shubhamkadam19@gmail.com>
2021-03-03 11:41:05 +0530

    Day 50

---
+++ Day50_context_managers.py
@@ -0,0 +1,18 @@
+class OpenFile:
+    def __init__(self,file,mode):
+        self.file=file
+        self.mode=mode
+
+    def __enter__(self):
+        self.f=open(self.file,self.mode)
+        return self.f
+    
+    def __exit__(self,exc_type,exc_val,exc_tb):
+        self.f.close()
+
+
+
+if __name__ == "__main__":
+    with OpenFile('sample.txt','r') as fobj:
+        print(fobj.readlines())
+
